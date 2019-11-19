66715f2b99c67299c613b9d906e2dd2f030952db
Yaroslav Souchkov <yaroslav.souchkov@gmail.com>
2019-11-19 17:46:14 +0300

    [+] DB schema v.0.0.c

--- database_schema/students_performance_monitoring.sql
+++ database_schema/students_performance_monitoring.sql
@@ -22,7 +22,7 @@ SET time_zone = "+00:00";
 -- База данных: `students_performance_monitoring`
 --
 
-DROP DATABASE `students_performance_monitoring`;
+DROP DATABASE IF EXISTS `students_performance_monitoring`;
 CREATE DATABASE IF NOT EXISTS `students_performance_monitoring` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `students_performance_monitoring`;
 
@@ -31,6 +31,12 @@ USE `students_performance_monitoring`;
 --
 -- Структура таблиц
 --
+
+CREATE TABLE `departments` (
+	`id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	`faculty_id` int(11) NOT NULL,
+	`name` varchar(120) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `educational_programs` (
 	`id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -47,8 +53,7 @@ CREATE TABLE IF NOT EXISTS `faculties` (
 
 CREATE TABLE IF NOT EXISTS `groups` (
   `id` varchar(10) NOT NULL PRIMARY KEY,
-  `specialization_id` int(11) NOT NULL,
-  `term_id` int(11) NOT NULL
+  `specialization_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -64,6 +69,7 @@ CREATE TABLE IF NOT EXISTS `records` (
   `subject_id` int(11) NOT NULL,
   `group_id` varchar(11) NOT NULL,
   `teacher_id` int(11) NOT NULL,
+  `term_id` int(11) NOT NULL,
   `control_type` enum('Зачёт','Дифференциальный зачёт','Экзамен','Курсовая работа') NOT NULL DEFAULT 'Зачёт',
   `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `mark_id` int(11) DEFAULT NULL,
@@ -87,9 +93,9 @@ CREATE TABLE IF NOT EXISTS `start_years` (
 CREATE TABLE IF NOT EXISTS `students` (
   `id` varchar(20) NOT NULL PRIMARY KEY COMMENT 'Gradeook number',
   `email` varchar(60) DEFAULT NULL,
-  `first_name` varchar(60) NOT NULL,
+  `first_name` varchar(30) NOT NULL,
   `fathers_name` varchar(60) DEFAULT NULL,
-  `last_name` varchar(60) NOT NULL,
+  `last_name` varchar(150) NOT NULL,
   `group_number` varchar(10) NOT NULL,
   `average_rating` float(1,1) NOT NULL,
   `update_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
@@ -98,6 +104,8 @@ CREATE TABLE IF NOT EXISTS `students` (
 
 CREATE TABLE IF NOT EXISTS `subjects` (
   `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `faculty_id` int(11) NOT NULL,
+  `department_id` int(11) NOT NULL,
   `name` varchar(120) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -116,12 +124,13 @@ CREATE TABLE `subjects_subject_blocks` (
 
 CREATE TABLE IF NOT EXISTS `teachers` (
   `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `user_id` int(11) NOT NULL,
   `login` varchar(60) NOT NULL,
   `password` varchar(60) NOT NULL,
   `email` varchar(60) DEFAULT NULL,
-  `first_name` varchar(60) NOT NULL,
+  `first_name` varchar(30) NOT NULL,
   `fathers_name` varchar(60) DEFAULT NULL,
-  `last_name` varchar(60) NOT NULL,
+  `last_name` varchar(150) NOT NULL,
   `degree` varchar(120) DEFAULT NULL,
   `token` varchar(120) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -133,6 +142,21 @@ CREATE TABLE IF NOT EXISTS `terms` (
   `number` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+
+CREATE TABLE `users` (
+	`id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	`password` varchar(128) NOT NULL,
+	`last_login` datetime NULL DEFAULT NULL,
+  `is_superuser` tinyint(1) NOT NULL DEFAULT 0,
+  `username` varchar(150) NOT NULL UNIQUE,
+  `first_name` varchar(30) NULL DEFAULT NULL,
+  `last_name` varchar(150) NULL DEFAULT NULL,
+  `email` varchar(254) NULL DEFAULT NULL,
+  `is_staff` tinyint(1) NOT NULL DEFAULT 0,
+  `is_active` tinyint(1) NOT NULL DEFAULT 1,
+  `date_joined` tinyint(1) NOT NULL DEFAULT CURRENT_TIMESTAMP
+  
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 -- --------------------------------------------------------
 
 --
@@ -144,8 +168,7 @@ ALTER TABLE `educational_programs`
 
 
 ALTER TABLE `groups`
-  ADD CONSTRAINT `groups_ibfk_1` FOREIGN KEY (`specialization_id`) REFERENCES `specializations` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `groups_ibfk_2` FOREIGN KEY (`term_id`) REFERENCES `terms` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `groups_ibfk_1` FOREIGN KEY (`specialization_id`) REFERENCES `specializations` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 
 ALTER TABLE `records`
@@ -153,7 +176,8 @@ ALTER TABLE `records`
   ADD CONSTRAINT `records_ibfk_2` FOREIGN KEY (`teacher_id`) REFERENCES `teachers` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `records_ibfk_3` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `records_ibfk_4` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `records_ibfk_5` FOREIGN KEY (`mark_id`) REFERENCES `marks` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `records_ibfk_5` FOREIGN KEY (`mark_id`) REFERENCES `marks` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `records_ibfk_6` FOREIGN KEY (`term_id`) REFERENCES `terms` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 
 ALTER TABLE `specializations`
@@ -164,9 +188,18 @@ ALTER TABLE `students`
   ADD CONSTRAINT `students_ibfk_1` FOREIGN KEY (`group_number`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 
+ALTER TABLE `subjects`
+  ADD CONSTRAINT `subjects_ibfk_1` FOREIGN KEY (`faculty_id`) REFERENCES `faculties` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `subjects_ibfk_2` FOREIGN KEY (`department_id`) REFERENCES `departments` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+
 ALTER TABLE `subjects_subject_blocks`
   ADD CONSTRAINT `subjects_subject_blocks_ibfk_1` FOREIGN KEY (`subject_id`) REFERENCES `subjects` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `subjects_subject_blocks_ibfk_2` FOREIGN KEY (`subject_block_id`) REFERENCES `subject_blocks` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+
+ALTER TABLE `teachers`
+  ADD CONSTRAINT `teachers_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 
 ALTER TABLE `terms`
