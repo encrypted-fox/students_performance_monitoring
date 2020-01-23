1e1a51d8345324336eff3c42446b608731e2d30f
Yaroslav Souchkov <yaroslav.souchkov@gmail.com>
2020-01-23 17:12:32 +0300

    [*] Updated settings.py v.0.1.d

--- students_performance_monitoring/settings.py
+++ students_performance_monitoring/settings.py
@@ -232,5 +232,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)/
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = "/usr/static/"
-STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
