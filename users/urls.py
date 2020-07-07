d087929afea3bfc0d5375b38fa01d47b1d720150
Yaroslav Souchkov <yaroslav.souchkov@gmail.com>
2020-07-07 20:36:36 +0300

    [+] Added users (previously – authentication) v.0.1.d

---
+++ users/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+
+from . import views
+
+urlpatterns = [
+    path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+    path('register/', views.register),
+    path('token/', views.token),
+    path('token/refresh/', views.refresh_token),
+    path('token/revoke/', views.revoke_token),
+]
