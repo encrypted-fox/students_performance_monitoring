cbecd2cfdc2e6a0eb435a99589d111748470beec
Yaroslav Souchkov <yaroslav.souchkov@gmail.com>
2020-07-07 20:32:45 +0300

    [*] Updated university_structure/models.py. Added table-naming v.0.1.e

--- university_structure/models.py
+++ university_structure/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 
 
@@ -7,11 +8,17 @@ class Faculties(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=120, null=False)
 
+    class Meta:
+        db_table = 'faculties'
+
 
 class EducationalPrograms(models.Model):
     id = models.AutoField(primary_key=True)
     faculty_id = models.ForeignKey(Faculties, on_delete=models.CASCADE, null=False)
     name = models.CharField(max_length=120, null=False)
+
+    class Meta:
+        db_table = 'educational_programs'
 
 
 class Specializations(models.Model):
@@ -19,14 +26,13 @@ class Specializations(models.Model):
     educational_program_id = models.ForeignKey(EducationalPrograms, on_delete=models.CASCADE, null=False)
     name = models.CharField(max_length=120, null=False)
 
+    class Meta:
+        db_table = 'specializations'
+
 
 class Groups(models.Model):
     id = models.CharField(max_length=10, primary_key=True, null=False)
     specialization_id = models.ForeignKey(Specializations, on_delete=models.CASCADE, null=False)
 
-
-
-
-
-
-
+    class Meta:
+        db_table = 'groups'
