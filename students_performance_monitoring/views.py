4a8658ca6d24cb2a4794740b5cb2e5c028fe9ccb
Yaroslav Souchkov <yaroslav.souchkov@gmail.com>
2020-07-14 14:08:19 +0300

    [*] Updated DocsView v.0.1.f

--- students_performance_monitoring/views.py
+++ students_performance_monitoring/views.py
@@ -10,37 +10,37 @@ class DocsView(APIView):
         api_docs = response = {
             "api/v0/": {
                 "documentation": {
-                    "swagger": request.build_absolute_uri("api/v0/swagger"),
-                    "redoc": request.build_absolute_uri("api/v0/redoc"),
+                    "swagger": request.build_absolute_uri("swagger"),
+                    "redoc": request.build_absolute_uri("redoc"),
                 },
-                "admin": request.build_absolute_uri("api/v0/admin"),
+                "admin": request.build_absolute_uri("admin"),
                 "authentication": {
-                    "auth/register": request.build_absolute_uri("api/v0/auth/register/"),
-                    "auth/token": request.build_absolute_uri("api/v0/auth/token/"),
-                    "auth/token/refresh": request.build_absolute_uri("api/v0/auth/token/refresh/"),
-                    "auth/token/revoke": request.build_absolute_uri("api/v0/auth/token/revoke/"),
+                    "auth/register": request.build_absolute_uri("auth/register/"),
+                    "auth/token": request.build_absolute_uri("auth/token/"),
+                    "auth/token/refresh": request.build_absolute_uri("auth/token/refresh/"),
+                    "auth/token/revoke": request.build_absolute_uri("auth/token/revoke/"),
                 },
                 "university_structure": {
-                    "faculties": request.build_absolute_uri("api/v0/faculties/"),
-                    "educational_programs": request.build_absolute_uri("api/v0/educational_programs/"),
-                    "specializations": request.build_absolute_uri("api/v0/specializations/"),
-                    "groups": request.build_absolute_uri("api/v0/groups/"),
+                    "faculties": request.build_absolute_uri("faculties/"),
+                    "educational_programs": request.build_absolute_uri("educational_programs/"),
+                    "specializations": request.build_absolute_uri("specializations/"),
+                    "groups": request.build_absolute_uri("groups/"),
                 },
                 "departments": {
-                    "departments": request.build_absolute_uri("api/v0/departments/"),
-                    "subjects": request.build_absolute_uri("api/v0/subjects/"),
-                    "subject_blocks": request.build_absolute_uri("api/v0/subject_blocks/"),
-                    "subjects_subject_blocks": request.build_absolute_uri("api/v0/subjects_subject_blocks/"),
+                    "departments": request.build_absolute_uri("departments/"),
+                    "subjects": request.build_absolute_uri("subjects/"),
+                    "subject_blocks": request.build_absolute_uri("subject_blocks/"),
+                    "subjects_subject_blocks": request.build_absolute_uri("subjects_subject_blocks/"),
                 },
                 "people": {
-                    "students": request.build_absolute_uri("api/v0/students/"),
-                    "teachers": request.build_absolute_uri("api/v0/teachers/"),
+                    "students": request.build_absolute_uri("students/"),
+                    "teachers": request.build_absolute_uri("teachers/"),
                 },
                 "information": {
-                    "terms": request.build_absolute_uri("api/v0/terms/"),
-                    "marks": request.build_absolute_uri("api/v0/marks/"),
-                    "control_types": request.build_absolute_uri("api/v0/control_types/"),
-                    "records": request.build_absolute_uri("api/v0/records/"),
+                    "terms": request.build_absolute_uri("terms/"),
+                    "marks": request.build_absolute_uri("marks/"),
+                    "control_types": request.build_absolute_uri("control_types/"),
+                    "records": request.build_absolute_uri("records/"),
                 },
             }
         }
