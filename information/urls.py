f41e0b21e624db5f07638f032d3bf4b84cb32a7a
Yaroslav Souchkov <yaroslav.souchkov@gmail.com>
2020-07-18 19:28:37 +0300

    [*] Fixed info routes v.0.1.h

--- information/urls.py
+++ information/urls.py
@@ -4,6 +4,7 @@ from .views import *
 
 router = routers.DefaultRouter()
 
+router.register('start_years', StartYearsViewSet)
 router.register('terms', TermsViewSet)
 router.register('marks', MarksViewSet)
 router.register('control_types', ControlTypesViewSet)
--- information/views.py
+++ information/views.py
@@ -3,6 +3,14 @@ from rest_framework import viewsets, permissions
 from .serializers import *
 
 
+class StartYearsViewSet(viewsets.ModelViewSet):
+    queryset = StartYears.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = StartYearsSerializer
+
+
 class TermsViewSet(viewsets.ModelViewSet):
     queryset = Terms.objects.all()
     permission_classes = [
