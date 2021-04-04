e32f5696654a4734e771d0872dce985b981dc38c
Yroslav Souchkov <yaroslav.souchkov@gmail.com>
2021-04-04 21:55:24 +0300

    fix basename

--- information/views.py
+++ information/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, permissions, authentication
+from rest_framework import viewsets, permissions, authentication, generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
