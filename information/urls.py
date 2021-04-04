3708ae06e5b83c3631531f3732473eacc23b641a
Yroslav Souchkov <yaroslav.souchkov@gmail.com>
2021-04-04 20:06:43 +0300

    fix basename

--- information/urls.py
+++ information/urls.py
@@ -8,6 +8,6 @@ router.register('start_years', StartYearsViewSet)
 router.register('terms', TermsViewSet)
 router.register('marks', MarksViewSet)
 router.register('control_types', ControlTypesViewSet)
-router.register('list_excellent_students', ListAllStudentsWithExcellentMarks)
+router.register('list_excellent_students', ListAllStudentsWithExcellentMarks, basename='ListAllStudentsWithExcellentMarks')
 
 urlpatterns = router.urls
