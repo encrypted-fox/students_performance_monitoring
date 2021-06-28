b3ce9aaf11a0571789d23f9f1accb885735fac75
Ярослав Сучков <ys.suchkov@sov-teh.com>
2021-06-28 17:35:58 +0300

    fix

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
@@ -55,7 +56,7 @@ def token(request):
         },
     )
     
-    return Response(r)
+    return Response(json.load(r))
     return Response(status=400)
 
 
