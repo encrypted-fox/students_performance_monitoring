28041ac5e387600894e7c14e74f9153c9b5286c1
Ярослав Сучков <ys.suchkov@sov-teh.com>
2021-07-01 14:16:20 +0300

    is_superuser

--- users/models.py
+++ users/models.py
@@ -92,6 +92,7 @@ class CustomUser(AbstractBaseUser):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)  # a admin user; non super-user
     is_admin = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
     settings = models.TextField(default="[]")
 
     USERNAME_FIELD = 'username'
